9df1850030180437a795e0c602d90606cb950b18
Arthur Plazanet <plazanet.art@gmail.com>
2026-02-26 22:12:25 +0100

    Fix lint

--- examples/runtime-showcase/app/components/IconOverview.vue
+++ examples/runtime-showcase/app/components/IconOverview.vue
@@ -2,14 +2,14 @@
   <input v-model="q" placeholder="Search icons…" />
   <YRow type="cluster" class="icon-overview">
     <YBox
+      v-for="x in filteredResolved"
+      :key="x.key"
       padding="xs"
       border="width-medium solid primary"
       background-color="neutral"
       width="auto"
-      v-for="x in filteredResolved"
-      :key="x.key"
     >
-      <Component :is="x.comp" size="lg" color="blue" strokeWidth="4" />
+      <Component :is="x.comp" size="lg" color="blue" stroke-width="4" />
     </YBox>
     <BoutonTwitterIcon size="2xl" color="red" stroke="blue" fill="green" stroke-width="87" />
   </YRow>
@@ -18,16 +18,9 @@
 <script setup lang="ts">
 import { useComposeIconRegistry } from '#imports';
 import { YBox, YRow } from '@use-compose/ui';
-import { computed, onMounted, type Component } from 'vue';
+import { computed, defineAsyncComponent, ref } from 'vue';
 
 const { icons, searchIcons } = useComposeIconRegistry();
-
-const iconsComponent = ref(
-  icons.map((icon) =>
-    defineAsyncComponent(() => import(`./icons/${icon.pascalName}`)),
-  ) as Component[],
-);
-icons;
 const q = ref('');
 
 const iconsList = computed(() => {
@@ -35,21 +28,11 @@ const iconsList = computed(() => {
   return query ? searchIcons(query) : icons;
 });
 
-const filteredResolved = computed(() =>
-  iconsList.value.map((icon) => ({
+const filteredResolved = computed(() => {
+  return iconsList.value.map((icon) => ({
     key: icon.pascalName,
     comp: defineAsyncComponent(() => import(`./icons/${icon.pascalName}`)),
-  })),
-);
-
-onMounted(() => {
-  ('Icons list:', iconsList.value);
-  ('Filtered resolved:', filteredResolved.value);
+  }));
 });
+// return
 </script>
-<style>
-.icon-overview {
-  /* display: grid;
-  gap: var(--gutter); */
-}
-</style>
--- packages/nuxt/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as IconsModule } from './src/module';
-export { useComposeIcon } from './src/runtime/composables/compose-icon';
-export type { UseComposeIcon } from './src/runtime/composables/compose-icon';
-export type { ComposeIconProps, ComposeIconSize, IconSizeKeyValue } from './src/runtime/types';
-export { getIconSizeClass } from './src/runtime/utils';
